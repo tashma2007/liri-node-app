51360e2c84f2a614b043b7f1a4e41d129c511f94
Natasha Henry <natashaphenry@yahoo.com>
2019-05-14 15:28:18 -0400

    added dependencies

--- liri.js
+++ liri.js
@@ -3,3 +3,12 @@ require("dotenv").config();
 var keys = require("./keys.js");
 
 var spotify = new Spotify(keys.spotify);
+
+var Spotify = require("node-spotify-api");
+
+var moment = require("moment");
+
+var axios = require("axios");
+
+var fs = require("fs");
+
