56ad617ed872cadff4b33c8f4083b5ff50c85adb
Natasha Henry <natashaphenry@yahoo.com>
2019-05-16 18:56:46 -0400

    added some code using inquirer

--- liri.js
+++ liri.js
@@ -2,9 +2,9 @@ require("dotenv").config();
 
 var keys = require("./keys.js");
 
-var spotify = new Spotify(keys.spotify);
+//var spotify = new Spotify(keys.spotify);
 
-var Spotify = require("node-spotify-api");
+//var Spotify = require("node-spotify-api");
 
 var moment = require("moment");
 
@@ -17,10 +17,13 @@ var inquirer = require('inquirer');
 
 inquirer.prompt([
     {
-
+type: "input",
+name: "concert",
+message: "What is the name of the artist or band that you would like to find an event for?"
 
     }
 
-]).then(function(location) {
-    
+]).then(function(answers) {
+    console.log(JSON.stringify(answers, null, ' '));
+
 })
