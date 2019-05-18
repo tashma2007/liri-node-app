9a69deb64a078d0665a63282f663a8793871dd67
Natasha Henry <natashaphenry@yahoo.com>
2019-05-17 22:13:52 -0400

    updates made to switch and inquirer prompt

--- liri.js
+++ liri.js
@@ -1,29 +1,102 @@
+// node pkgs and keys
 require("dotenv").config();
-
-var keys = require("./keys.js");
-
-//var spotify = new Spotify(keys.spotify);
-
-//var Spotify = require("node-spotify-api");
-
-var moment = require("moment");
-
-var axios = require("axios");
 
 var fs = require("fs");
 
 var inquirer = require('inquirer');
 
+var axios = require("axios");
+
+var moment = require("moment");
+
+var keys = require("./keys.js");
+
+//var Spotify = require("node-spotify-api");
+
+//var spotify = new Spotify(keys.spotify);
+
+
 
 inquirer.prompt([
     {
-type: "input",
-name: "concert",
-message: "What is the name of the artist or band that you would like to find an event for?"
+        type: "list",
+        name: "entertainment",
+        message: "Hi! My name is Liri-Bot. Please choose from one of the following choices so that I may assist you:",
+        choices: ["concert-this", "spotify-this-song", "movie-this", "do-what-it-says"]
+    },
+]).then(function (inquirerResponse) {
+    //console.log(inquirerResponse)
 
+    switch (inquirerResponse) {
+        case "concert-this":
+            concertThis();
+            break;
+        case "spotify-this-song":
+            spotifyThis();
+            break;
+        case "movie-this":
+            movieThis();
+            break;
+        case "do-what-it-says":
+            doWhatItSays();
+            break;
+        default:
     }
 
-]).then(function(answers) {
-    console.log(JSON.stringify(answers, null, ' '));
+    function concertThis() {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "concert-this",
+                message: "What is the name of the artist or band that you would like to find an event for?"
 
-})
+            },
+        ]).then(function (inquirerResponse) {
+            axios.get("https://rest.bandsintown.com/artists/" + inquirerResponse.band + "/events?app_id=codingbootcamp")
+                .then(function (response) {
+                    for (var i = 0; i < 5; i++) {
+                        console.log(inquirerResponse)
+                    }
+                })
+            })
+        };
+
+
+
+
+
+
+
+
+    //{
+        //type: "input",
+        //name: "concert",
+        //message: "What is the name of the artist or band that you would like to find an event for?"
+
+    //},
+
+    //{
+        //type: "input",
+        //name: "song",
+        //message: "What is the name of the song that you are searching for?"
+
+    //},
+
+    //{
+        //type: "input",
+        //name: "movie",
+        //message: "What is the name of the movie that you are searching for?"
+
+
+    //},
+
+   //{
+        //type: "input",
+        //name: "concert",
+        //message: "What is the name of the artist or band that you would like to find an event for?"
+
+    //}
+
+        
+
+});
